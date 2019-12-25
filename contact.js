a849c202838778b5187de050bc1dcd933ada8aec
juju669 <58565920+juju669@users.noreply.github.com>
2019-12-24 19:07:18 -0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,40 @@
+//Name: Julian Wilson
+//Date Created: 12/16/19
+//Most recent revision
+
+
+
+
+function clearErrors() {
+    for (var loopCounter = 0;
+        loopCounter < document.forms["contactInfo"].elements.length;
+        loopCounter++) {
+        if (document.forms["contactInfo"].elements[loopCounter]
+           .parentElement.className.indexOf("has-") != -1) {
+
+            document.forms["contactInfo"].elements[loopCounter]
+               .parentElement.className = "form-group";
+        }
+    }
+}
+
+function resetForm() {
+    clearErrors();
+    document.forms["contactInfo"]["name"].value = "";
+    document.forms["contactInfo"]["email"].value = "";
+    document.forms["contactInfo"]["phone"].value = "";
+    document.getElementById("sendRequest").innerText = "Send Request";
+}
+
+function validateItems() {
+    clearErrors();
+    var name = document.forms["contactInfo"]["name"].value;
+    var email = document.forms["contactInfo"]["email"].value;
+    var phone = document.forms["contactInfo"]["phone"].value;
+    if (name == "" || email =="" || phone =="") {
+        alert("Please Complete Empty Fields.");
+    }
+    else {
+      return confirm("PLEASE CONFIRM: " + "NAME: " + name + "  " + "EMAIL: " + email + "  " + "PHONE: " + phone);;
+    }
+}
